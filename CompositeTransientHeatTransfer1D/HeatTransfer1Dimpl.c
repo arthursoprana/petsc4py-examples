87006912b083be2069211ecd79a116d7a61ef521
Arthur Soprano <arthursoprano@gmail.com>
2016-11-19 16:08:29 -0200

    Update implementation to use arrays instead of ellipsis operator.

--- CompositeTransientHeatTransfer1D/HeatTransfer1Dimpl.c
+++ CompositeTransientHeatTransfer1D/HeatTransfer1Dimpl.c
@@ -1,4 +1,5 @@
 #include "HeatTransfer1Dimpl.h"
+#include "include_patch_pack.h"
 
 #undef __FUNCT__
 #define __FUNCT__ "FormInitGuess"
@@ -9,22 +10,22 @@ Unwraps the global solution vector and passes its local pieces into the user fun
 */
 PetscErrorCode FormInitGuess(DM dm, Vec X, Params *p)
 {
-	PetscErrorCode ierr;
-	Vec            X1, X2;
-	DM             da1, da2;
+    PetscErrorCode ierr;
+    Vec            X1, X2;
+    DM             da1, da2;
 
-	PetscFunctionBegin;
-	ierr = DMCompositeGetEntries(dm, &da1, &da2); CHKERRQ(ierr);
+    PetscFunctionBegin;
+    ierr = DMCompositeGetEntries(dm, &da1, &da2); CHKERRQ(ierr);
 
-	/* Access the subvectors in X */
-	ierr = DMCompositeGetAccess(dm, X, &X1, &X2); CHKERRQ(ierr);
+    /* Access the subvectors in X */
+    ierr = DMCompositeGetAccess(dm, X, &X1, &X2); CHKERRQ(ierr);
 
-	/* Evaluate local user provided function */
-	ierr = FormInitGuessLocal(da1, X1, p); CHKERRQ(ierr);
-	ierr = FormInitGuessLocal(da2, X2, p); CHKERRQ(ierr);
+    /* Evaluate local user provided function */
+    ierr = FormInitGuessLocal(da1, X1, p); CHKERRQ(ierr);
+    ierr = FormInitGuessLocal(da2, X2, p); CHKERRQ(ierr);
 
-	ierr = DMCompositeRestoreAccess(dm, X, &X1, &X2); CHKERRQ(ierr);
-	PetscFunctionReturn(0);
+    ierr = DMCompositeRestoreAccess(dm, X, &X1, &X2); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
 }
 
 #undef __FUNCT__
@@ -110,18 +111,18 @@ PetscErrorCode FormFunctionLocal1(DMDALocalInfo *info, PetscReal t, Field *x1, F
         f[i].T  = x1[i].T - T_left;
     }
 
-	// Not sure why we need this :(
-	if (xints == 0) {
-		xints = xints + 1;
-	}
-	if (xinte == info->mx) {
-		xinte = xinte - 1;
-		i = info->mx - 1;
-		/* Temperature */
-		f[i].T = x_t[i].T * dx
-			   - 1.0 * (+k * (x2[0].T - x1[i].T) / dx
-				        -k * (x1[i].T - x1[i - 1].T) / dx + Q * dx);
-	}
+    // Not sure why we need this :(
+    if (xints == 0) {
+        xints = xints + 1;
+    }
+    if (xinte == info->mx) {
+        xinte = xinte - 1;
+        i = info->mx - 1;
+        /* Temperature */
+        f[i].T = x_t[i].T * dx
+               - 1.0 * (+k * (x2[0].T - x1[i].T) / dx
+                        -k * (x1[i].T - x1[i - 1].T) / dx + Q * dx);
+    }
 
     /* Compute over the interior points */
     for (i=xints; i<xinte; i++) {
@@ -141,59 +142,60 @@ PetscErrorCode FormFunctionLocal1(DMDALocalInfo *info, PetscReal t, Field *x1, F
 #define __FUNCT__ "FormFunctionLocal2"
 PetscErrorCode FormFunctionLocal2(DMDALocalInfo *info, PetscReal t, Field *x1, Field *x2, Field *x_t, Field *f, Params *p, PetscInt comp_offset, PetscInt total_size)
 {
-	PetscErrorCode ierr;
-	PetscInt       xints, xinte, i;
-	PetscReal      dx;
-	PetscReal      L, k, conductivity, T_left, T_right, Q;
+    PetscErrorCode ierr;
+    PetscInt       xints, xinte, i;
+    PetscReal      dx;
+    PetscReal      L, k, conductivity, T_left, T_right, Q;
 
-	PetscFunctionBegin;
-	conductivity = p->conductivity_;
-	T_left = p->temperature_left_;
-	T_right = p->temperature_right_;
-	Q = p->source_term_;
-	L = p->wall_length_;
-	/*
-	Define mesh intervals ratios for uniform grid.
-	*/
+    PetscFunctionBegin;
+    conductivity = p->conductivity_;
+    T_left = p->temperature_left_;
+    T_right = p->temperature_right_;
+    Q = p->source_term_;
+    L = p->wall_length_;
+    /*
+    Define mesh intervals ratios for uniform grid.
+    */
 
-	dx = L / (PetscReal)(total_size - 1);
-	k = conductivity;
+    dx = L / (PetscReal)(total_size - 1);
+    k = conductivity;
 
-	xints = info->xs; xinte = info->xs + info->xm;
+    xints = info->xs; xinte = info->xs + info->xm;
 
-	/* Test whether we are on the right edge of the global array */
-	if (xinte + comp_offset == total_size) {
-		i = info->mx - 1;
-		/* right edge */
-		f[i].T = x2[i].T - T_right;
-	}
+    /* Test whether we are on the right edge of the global array */
+    if (xinte + comp_offset == total_size) {
+        i = info->mx - 1;
+        /* right edge */
+        f[i].T = x2[i].T - T_right;
+    }
 
-	// Not sure why we need this :(
-	if (xints == 0) {
-		xints = xints + 1;
-		i = 0;
-		/* Temperature */
-		f[i].T = x_t[i].T * dx
-			- 1.0 * (+k * (x2[i+1].T - x2[i].T) / dx
-				     -k * (x2[i].T - x1[info->mx - 1].T) / dx + Q * dx);
-	}
-	if (xinte == info->mx) {
-		xinte = xinte - 1;
-	}
+    // Not sure why we need this :(
+    if (xints == 0) {
+        xints = xints + 1;
+        i = 0;
+        /* Temperature */
+        f[i].T = x_t[i].T * dx
+            - 1.0 * (+k * (x2[i+1].T - x2[i].T) / dx
+                     -k * (x2[i].T - x1[info->mx - 1].T) / dx + Q * dx);
+    }
+    if (xinte == info->mx) {
+        xinte = xinte - 1;
+    }
 
-	/* Compute over the interior points */
-	for (i = xints; i<xinte; i++) {
-		/* Temperature */
-		f[i].T = x_t[i].T * dx
-			   - 1.0 * (+k * (x2[i + 1].T - x2[i].T) / dx
-				        -k * (x2[i].T - x2[i - 1].T) / dx + Q * dx);
-	}
-	/*
-	Flop count (multiply-adds are counted as 2 operations)
-	*/
-	ierr = PetscLogFlops(84.0*info->xm); CHKERRQ(ierr);
-	PetscFunctionReturn(0);
+    /* Compute over the interior points */
+    for (i = xints; i<xinte; i++) {
+        /* Temperature */
+        f[i].T = x_t[i].T * dx
+               - 1.0 * (+k * (x2[i + 1].T - x2[i].T) / dx
+                        -k * (x2[i].T - x2[i - 1].T) / dx + Q * dx);
+    }
+    /*
+    Flop count (multiply-adds are counted as 2 operations)
+    */
+    ierr = PetscLogFlops(84.0*info->xm); CHKERRQ(ierr);
+    PetscFunctionReturn(0);
 }
+
 
 #undef __FUNCT__
 #define __FUNCT__ "FormFunction"
@@ -201,77 +203,73 @@ PetscErrorCode FormFunction(TS ts, PetscReal t, Vec X, Vec X_t, Vec F, Params *p
 {
     DMDALocalInfo  info;
     Field          *u1, *u2;
-	Field          *u1_t, *u2_t;
-	Field          *f1, *f2;
+    Field          *u1_t, *u2_t;
+    Field          *f1, *f2;
     PetscErrorCode ierr;
-	Vec            X1, X2;
-	Vec            X1_t, X2_t;
-	Vec            F1, F2;
     DM             dm;
-	DM             da1, da2;
+    PetscInt nDM;
+
+    // TODO: Find a way to dynamically allocate these arrays in c
+    DM  das[2];
+    Vec Xs[2], X_ts[2], Fs[2];
+    Field* u[2];
+    Field* u_t[2];
+    Field* f[2];
 
     PetscFunctionBegin;
 
-    ierr = TSGetDM(ts, &dm); CHKERRQ(ierr);
-		
-	ierr = DMCompositeGetEntries(dm, &da1, &da2); CHKERRQ(ierr);
+    ierr = TSGetDM(ts, &dm); CHKERRQ(ierr);    
+    ierr = DMCompositeGetNumberDM(dm, &nDM); CHKERRQ(ierr);
+    ierr = DMCompositeGetEntriesArray(dm, das); CHKERRQ(ierr);
+    
+    /* Access the subvectors in X */    
+    ierr = DMCompositeGetLocalVectorsArray(dm, nDM, NULL, Xs); CHKERRQ(ierr);
+    ierr = DMCompositeScatterArray(dm, X, Xs); CHKERRQ(ierr);
+    
+    ierr = DMCompositeGetLocalVectorsArray(dm, nDM, NULL, X_ts); CHKERRQ(ierr);
+    ierr = DMCompositeScatterArray(dm, X_t, X_ts); CHKERRQ(ierr);
 
-	/* Access the subvectors in X */
-	ierr = DMCompositeGetLocalVectors(dm, &X1, &X2); CHKERRQ(ierr);
-	ierr = DMCompositeScatter(dm, X, X1, X2); CHKERRQ(ierr);
+    // Calculate total size
+    PetscInt total_size = 0;
+    for (int i = 0; i < nDM; ++i) {
+        ierr = DMDAGetLocalInfo(das[i], &info); CHKERRQ(ierr);
+        total_size += info.mx;
+    }
 
-	ierr = DMCompositeGetLocalVectors(dm, &X1_t, &X2_t); CHKERRQ(ierr);
-	ierr = DMCompositeScatter(dm, X_t, X1_t, X2_t); CHKERRQ(ierr);
-	
-	PetscInt nDM;
-	DMCompositeGetNumberDM(dm, &nDM);
+    /* Access the subvectors in F.
+    These are not ghosted so directly access the memory locations in F */
+    ierr = DMCompositeGetAccessArray(dm, F, nDM, NULL, Fs); CHKERRQ(ierr);
 
-	PetscInt total_size = 0;
+    for (int i = 0; i < nDM; ++i) {
+        ierr = DMDAVecGetArray(das[i], X_ts[i], &(u_t[i])); CHKERRQ(ierr);
+        ierr = DMDAVecGetArray(das[i], Xs[i], &(u[i])); CHKERRQ(ierr);
+        ierr = DMDAVecGetArray(das[i], Fs[i], &(f[i])); CHKERRQ(ierr);
+    }
 
-	ierr = DMDAGetLocalInfo(da1, &info); CHKERRQ(ierr);
-	total_size += info.mx;
-	ierr = DMDAGetLocalInfo(da2, &info); CHKERRQ(ierr);
-	total_size += info.mx;
+    PetscInt comp_offset = 0;
 
-	/* Access the subvectors in F.
-	These are not ghosted so directly access the memory locations in F */
-	ierr = DMCompositeGetAccess(dm, F, &F1, &F2); CHKERRQ(ierr);
+    // Obs for fluxes we need all u1 and u2 !
+    ///////////////////////////////////////////////////////
+    // da1
+    ierr = DMDAGetLocalInfo(das[0], &info); CHKERRQ(ierr);
+    ierr = FormFunctionLocal1(&info, t, u[0], u[1], u_t[0], f[0], p, comp_offset, total_size); CHKERRQ(ierr);
+    comp_offset += info.mx;
+    ///////////////////////////////////////////////////////
+    // da2
+    ierr = DMDAGetLocalInfo(das[1], &info); CHKERRQ(ierr);
+    ierr = FormFunctionLocal2(&info, t, u[0], u[1], u_t[1], f[1], p, comp_offset, total_size); CHKERRQ(ierr);
+    comp_offset += info.mx;
+    ///////////////////////////////////////////////////////
 
-    ierr = DMDAVecGetArray(da1, X1, &u1); CHKERRQ(ierr);
-    ierr = DMDAVecGetArray(da1, X1_t, &u1_t); CHKERRQ(ierr);
-    ierr = DMDAVecGetArray(da1, F1, &f1); CHKERRQ(ierr);
+    for (int i = 0; i < nDM; ++i) {
+        ierr = DMDAVecRestoreArray(das[i], X_ts[i], &(u_t[i])); CHKERRQ(ierr);
+        ierr = DMDAVecRestoreArray(das[i], Xs[i], &(u[i])); CHKERRQ(ierr);
+        ierr = DMDAVecRestoreArray(das[i], Fs[i], &(f[i])); CHKERRQ(ierr);
+    }
 
-	ierr = DMDAVecGetArray(da2, X2, &u2); CHKERRQ(ierr);
-	ierr = DMDAVecGetArray(da2, X2_t, &u2_t); CHKERRQ(ierr);
-	ierr = DMDAVecGetArray(da2, F2, &f2); CHKERRQ(ierr);
-
-	PetscInt comp_offset = 0;
-
-	// Obs for fluxes we need all u1 and u2 !
-	///////////////////////////////////////////////////////
-	// da1
-	ierr = DMDAGetLocalInfo(da1, &info); CHKERRQ(ierr);
-    ierr = FormFunctionLocal1(&info, t, u1, u2, u1_t, f1, p, comp_offset, total_size); CHKERRQ(ierr);
-	comp_offset += info.mx;
-	///////////////////////////////////////////////////////
-	// da2
-	ierr = DMDAGetLocalInfo(da2, &info); CHKERRQ(ierr);
-	ierr = FormFunctionLocal2(&info, t, u1, u2, u2_t, f2, p, comp_offset, total_size); CHKERRQ(ierr);
-	comp_offset += info.mx;
-	///////////////////////////////////////////////////////
-
-    ierr = DMDAVecRestoreArray(da1, X1, &u1); CHKERRQ(ierr);
-    ierr = DMDAVecRestoreArray(da1, X1_t, &u1_t); CHKERRQ(ierr);
-	ierr = DMDAVecRestoreArray(da1, F1, &f1); CHKERRQ(ierr);
-
-	ierr = DMDAVecRestoreArray(da2, X2, &u2); CHKERRQ(ierr);
-	ierr = DMDAVecRestoreArray(da2, X2_t, &u2_t); CHKERRQ(ierr);	
-	ierr = DMDAVecRestoreArray(da2, F2, &f2); CHKERRQ(ierr);
-
-	ierr = DMCompositeRestoreAccess(dm, F, &F1, &F2); CHKERRQ(ierr);
-
-	ierr = DMCompositeRestoreLocalVectors(dm, &X1, &X2); CHKERRQ(ierr);
-	ierr = DMCompositeRestoreLocalVectors(dm, &X1_t, &X2_t); CHKERRQ(ierr);
+    ierr = DMCompositeRestoreAccessArray(dm, F, nDM, NULL, Fs); CHKERRQ(ierr);
+    ierr = DMCompositeRestoreLocalVectorsArray(dm, nDM, NULL, Xs); CHKERRQ(ierr);
+    ierr = DMCompositeRestoreLocalVectorsArray(dm, nDM, NULL, X_ts); CHKERRQ(ierr);
 
     PetscFunctionReturn(0);
 }
@@ -280,8 +278,8 @@ PetscErrorCode FormFunction(TS ts, PetscReal t, Vec X, Vec X_t, Vec F, Params *p
 #undef __FUNCT__
 #define __FUNCT__ "CompositeSetCoupling"
 PetscErrorCode CompositeSetCoupling(DM dm) {
-	PetscErrorCode ierr = DMCompositeSetCoupling(dm, FormCoupleLocations);
-	return ierr;
+    PetscErrorCode ierr = DMCompositeSetCoupling(dm, FormCoupleLocations);
+    return ierr;
 }
 
 
@@ -292,42 +290,42 @@ Computes the coupling between DA1 and DA2. This determines the location of each 
 */
 PetscErrorCode FormCoupleLocations(DM dm, Mat A, PetscInt *dnz, PetscInt *onz, PetscInt __rstart, PetscInt __nrows, PetscInt __start, PetscInt __end)
 {
-	PetscInt       cols[3], row;
-	PetscErrorCode ierr;
-	DM             da1, da2;
+    PetscInt       cols[3], row;
+    PetscErrorCode ierr;
+    DM             da1, da2;
 
-	PetscFunctionBegin;
+    PetscFunctionBegin;
 
-	ierr = DMCompositeGetEntries(dm, &da1, &da2); CHKERRQ(ierr);	
-	DMDALocalInfo  info1, info2;
-	ierr = DMDAGetLocalInfo(da1, &info1); CHKERRQ(ierr);
-	ierr = DMDAGetLocalInfo(da2, &info2); CHKERRQ(ierr);
-	int size = info1.mx + info2.mx;
+    ierr = DMCompositeGetEntries(dm, &da1, &da2); CHKERRQ(ierr);    
+    DMDALocalInfo  info1, info2;
+    ierr = DMDAGetLocalInfo(da1, &info1); CHKERRQ(ierr);
+    ierr = DMDAGetLocalInfo(da2, &info2); CHKERRQ(ierr);
+    int size = info1.mx + info2.mx;
 
-	// Hack: Bug in petsc file -> packm.c @ line (173) and line (129)
-	// First A is NULL, then later dnz and onz are NULL, that's why
-	// we need an IF here.
-	if (!A) {
-		cols[0] = info1.mx;
-		row = info1.mx - 1;
-		ierr = MatPreallocateLocation(A, row, 1, cols, dnz, onz); CHKERRQ(ierr);
-		cols[0] = info1.mx - 1;
-		row = info1.mx;
-		ierr = MatPreallocateLocation(A, row, 1, cols, dnz, onz); CHKERRQ(ierr);
-	}
-	else {
-		PetscScalar values[1];
+    // Hack: Bug in petsc file -> packm.c @ line (173) and line (129)
+    // First A is NULL, then later dnz and onz are NULL, that's why
+    // we need an IF here.
+    if (!A) {
+        cols[0] = info1.mx;
+        row = info1.mx - 1;
+        ierr = MatPreallocateLocation(A, row, 1, cols, dnz, onz); CHKERRQ(ierr);
+        cols[0] = info1.mx - 1;
+        row = info1.mx;
+        ierr = MatPreallocateLocation(A, row, 1, cols, dnz, onz); CHKERRQ(ierr);
+    }
+    else {
+        PetscScalar values[1];
 
-		row = info1.mx - 1;
-		cols[0] = info1.mx;
-		values[0] = 0.0;
-		ierr = MatSetValues(A, 1, &row, 1, cols, values, INSERT_VALUES); CHKERRQ(ierr);
+        row = info1.mx - 1;
+        cols[0] = info1.mx;
+        values[0] = 0.0;
+        ierr = MatSetValues(A, 1, &row, 1, cols, values, INSERT_VALUES); CHKERRQ(ierr);
 
-		row = info1.mx;
-		cols[0] = info1.mx - 1;
-		values[0] = 0.0;
-		ierr = MatSetValues(A, 1, &row, 1, cols, values, INSERT_VALUES); CHKERRQ(ierr);
-	}
-	
-	PetscFunctionReturn(0);
+        row = info1.mx;
+        cols[0] = info1.mx - 1;
+        values[0] = 0.0;
+        ierr = MatSetValues(A, 1, &row, 1, cols, values, INSERT_VALUES); CHKERRQ(ierr);
+    }
+    
+    PetscFunctionReturn(0);
 }
---
+++ CompositeTransientHeatTransfer1D/main.py
@@ -0,0 +1,128 @@
+import sys
+import numpy as np
+import petsc4py
+from petsc4py import PETSc
+from matplotlib import pyplot as plt
+import HeatTransfer1D
+
+petsc4py.init(sys.argv)
+
+def transient_heat_transfer_1D(
+    nx, temperature_left, 
+    temperature_right, 
+    conductivity,
+    source_term,
+    wall_length,
+    final_time,
+    initial_time_step
+    ):
+    
+    # Time Stepper (TS) for ODE and DAE
+    # DAE - https://en.wikipedia.org/wiki/Differential_algebraic_equation
+    # https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/
+    ts = PETSc.TS().create()
+
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/DM/index.html
+    #
+    da1 = PETSc.DMDA().create([nx],dof=1, stencil_width=1, stencil_type='star')
+    da2 = PETSc.DMDA().create([nx],dof=1, stencil_width=1, stencil_type='star')
+    
+    dm = PETSc.DMComposite().create()
+    dm.addDM(da1)
+    dm.addDM(da2)
+    HeatTransfer1D.compositeSetCoupling(dm)
+    
+    ts.setDM(dm)
+
+    F = dm.createGlobalVec()
+
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetIFunction.html
+    ts.setIFunction(HeatTransfer1D.formFunction, F,
+                     args=(temperature_left, temperature_right, conductivity, source_term, wall_length))
+
+    x = dm.createGlobalVec()
+    
+    HeatTransfer1D.formInitGuess(x, dm, temperature_left, temperature_right, conductivity, source_term, wall_length)
+
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetDuration.html
+    ts.setDuration(max_time=final_time, max_steps=None)
+    
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetExactFinalTime.html
+    ts.setExactFinalTime(ts.ExactFinalTimeOption.STEPOVER)
+    
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetInitialTimeStep.html
+    ts.setInitialTimeStep(initial_time=0.0, initial_time_step=initial_time_step)
+    
+    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetProblemType.html
+    ts.setProblemType(ts.ProblemType.NONLINEAR)
+    
+    # Another way to set the solve type is through PETSc.Options()
+    #ts.setType(ts.Type.CRANK_NICOLSON)
+    #ts.setType(ts.Type.THETA)
+    #ts.setTheta(theta=0.9999)
+    #ts.setType(ts.Type.EIMEX) # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSEIMEX.html
+    #ts.setType(ts.Type.BDF      )
+
+    ts.setFromOptions()
+
+    ts.solve(x)
+
+    return x
+
+options = PETSc.Options()
+options.clear()
+
+dt = 0.001                 # [s]
+dt_min = 1e-4              # [s]
+dt_max = 0.1               # [s]
+
+#ts_type = "beuler"
+#ts_type = "pseudo" # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSPSEUDO.html
+#ts_type = "cn"
+ts_type = "bdf"
+options.setValue('-ts_type', ts_type)
+
+options.setValue('-ts_bdf_order', 3) # https://en.wikipedia.org/wiki/Backward_differentiation_formula
+options.setValue('-ts_adapt_type', 'basic') # basic or none
+options.setValue('-ts_bdf_adapt', '')
+options.setValue('-ts_adapt_dt_min', dt_min)
+options.setValue('-ts_adapt_dt_max', dt_max)
+options.setValue('-ts_monitor', None)
+
+dense = False
+if dense:
+    options.setValue('-dmcomposite_dense_jacobian', None)
+else:
+    options.delValue('-dmcomposite_dense_jacobian')
+
+options.setValue('-ts_fd_color', None)
+#options.delValue('-ts_bdf_adapt')
+
+#options.setValue('-is_coloring_view', '')
+
+nx = 50
+temperature_left  = 0.0    # [degC]
+temperature_right = 50.0   # [degC]
+conductivity = 1.0         # [W/(m.K)]
+source_term = 100.0          # [W/m3]
+wall_length = 1.0          # [m]
+
+time_intervals = [0.001, 0.01, 0.05, 0.1, 1.0]
+#time_intervals = [0.001]
+sols = []
+for final_time in time_intervals:
+    sol = transient_heat_transfer_1D(
+        nx, temperature_left, 
+        temperature_right, 
+        conductivity,
+        source_term,
+        wall_length,
+        final_time,
+        dt
+        )
+    sols.append(sol[...])
+    
+x = np.linspace(0, wall_length, 2*nx)
+for sol in sols:
+    plt.plot(x, sol)
+plt.show()
