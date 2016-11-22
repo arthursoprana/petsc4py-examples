1cc0f980d8c6a7dce567b64aae7ab542e9691fad
Arthur Soprano <arthur@esss.com.br>
2016-11-22 13:32:38 -0200

    add python implementation of residual function

--- CompositeTransientHeatTransfer1D/main.py
+++ CompositeTransientHeatTransfer1D/main.py
@@ -5,7 +5,60 @@ from petsc4py import PETSc
 from matplotlib import pyplot as plt
 import HeatTransfer1D
 
+
+
 petsc4py.init(sys.argv)
+
+
+class Heat(object):
+    def __init__(self, dm, temperature_presc, conductivity, source_term, wall_length):
+
+        self.dm = dm 
+        self.k  = conductivity
+        self.Q  = source_term
+        self.L  = wall_length
+        self.Tpresc = temperature_presc
+
+    def evalFunction(self, ts, t, x, xdot, f):
+        dm = self.dm
+        Tpresc = self.Tpresc
+        k  = self.k
+        Q  = self.Q 
+        L  = self.L
+
+        with dm.getAccess(x, locs=None) as Xs:
+            with dm.getAccess(xdot, locs=None) as X_ts:
+                with dm.getAccess(f, locs=None) as Fs:
+                    dx_avg = 0.0
+                    size = len(Fs)
+                    Fs[size-1][0] = 0.0
+                    
+                    for X, X_t, F, T in zip(Xs[:-1], X_ts[:-1], Fs[:-1], Tpresc):   
+                        udot = X_t
+                        u = X            
+                        
+                        dx = L / F.size
+                        ff = np.zeros(F.size)
+                        ff[1:-1] = udot[1:-1]*dx - Q*dx + k * (u[1:-1] -  u[:-2])/dx - k * (u[2:] - u[1:-1])/dx
+                        
+                        ff[ 0] = udot[       0]*dx - Q*dx - k * (u[       1] - u[       0])/dx
+                        ff[-1] = udot[u.size-1]*dx - Q*dx + k * (u[u.size-1] - u[u.size-2])/dx
+                        
+                        # Internal Node
+                        flux_term = k * (Xs[size-1][0] - u[u.size-1]) / dx
+                        Fs[size-1][0] += +flux_term
+                        ff[-1]  += -flux_term
+                        
+                        # BC's
+                        ff[0] += +k * (u[0] - T) / (0.5*dx)
+                        
+                        F.setArray(ff)
+                        dx_avg += dx
+                    dx_avg /= len(Xs[:-1])
+                    
+                    Fs[size-1][0] += udot[size-1]*dx_avg - Q*dx_avg
+   
+
 
 def transient_heat_transfer_1D(
     npipes, nx, 
@@ -15,7 +68,8 @@ def transient_heat_transfer_1D(
     source_term,
     wall_length,
     final_time,
-    initial_time_step
+    initial_time_step,
+    impl_python=False
     ):
     
     # Time Stepper (TS) for ODE and DAE
@@ -51,10 +105,14 @@ def transient_heat_transfer_1D(
 
     F = dm.createGlobalVec()
 
-    # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetIFunction.html
-    ts.setIFunction(HeatTransfer1D.formFunction, F,
-                     args=(conductivity, source_term, wall_length, temperature_presc))
-
+    if impl_python:        
+        ode = Heat(dm, temperature_presc, conductivity, source_term, wall_length)
+        ts.setIFunction(ode.evalFunction, F)
+    else:
+        # http://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/TS/TSSetIFunction.html
+        ts.setIFunction(HeatTransfer1D.formFunction, F,
+                         args=(conductivity, source_term, wall_length, temperature_presc))    
+    
     x = dm.createGlobalVec()
     
     x[...] = initial_temperature
@@ -117,17 +175,18 @@ options.setValue('-ts_fd_color', None)
 #options.setValue('-mat_view', 'draw')
 #options.setValue('-draw_pause', 100)
 #options.setValue('-is_coloring_view', '')
+options.setValue('-help', None)
 
 npipes = 4
-nx = 100
-temperature_presc = np.array([2.0, 50.0, 50.0, 50.0]) # [degC]
-initial_temperature = 0.0 # [degC]
+nx = 50
+temperature_presc = np.array([2.0, 50.0, 55.0, 60.0]) # [degC]
+initial_temperature = 10.0 # [degC]
 conductivity = 1.0         # [W/(m.K)]
 source_term = 00.0          # [W/m3]
 wall_length = 1.0          # [m]
 
-time_intervals = [0.001, 0.01, 0.05, 0.1, 1.0]
-#time_intervals = [0.001]
+#time_intervals = [0.001, 0.01, 0.05, 0.1, 1.0]
+time_intervals = [0.5]
 sols = []
 for final_time in time_intervals:
     sol = transient_heat_transfer_1D(
@@ -138,7 +197,8 @@ for final_time in time_intervals:
         source_term,
         wall_length,
         final_time,
-        dt
+        dt,
+        impl_python=True
         )
     sols.append(sol[...])
     
